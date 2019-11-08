380148aabd1d432a2f1dce2fc93dd8465351a1b6
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-11-08 21:22:36 +0200

    Added default zone.

--- connect.sh
+++ connect.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Startig to create instances..."
+ZONE=europe-west1-d
 for i in {0..5}
 do
-   gcloud compute instances create --image-family centos-8 --image-project=centos-cloud centos-test-$i
+   gcloud compute instances create --zone $ZONE --image-family centos-8 --image-project=centos-cloud centos-test-$i
 done
 echo "Complete."
