1ff6c0991dbee51ae301f65615f44e2b1e7f2b81
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-11-09 02:34:46 +0200

    Refactored the client code.

--- heartbeat_client.py
+++ heartbeat_client.py
@@ -12,33 +12,34 @@ class HeartbeatClient:
     HEARTBEAT_INCREMENT = 1
 
     def __init__(self):
-        self.heartbeat_counter = 0
-
+        self.heartbeat_counter = {}
+        self.server_list = sys.argv[1:]
+        for server in self.server_list:
+            self.heartbeat_counter[server] = 0
         logging.basicConfig(format='%(levelname)s: %(asctime)s %(message)s', level=logging.INFO)
 
     def start_client(self):
         while 1:
-            try:
-                self.set_connection(server_list=sys.argv[1:])
-            except ConnectionRefusedError:
-                pass
-            except KeyboardInterrupt:
-                break
+            self.set_connection()
 
             time.sleep(self.HEARTBEAT_INCREMENT)
-            self.heartbeat_counter += 1
 
-    def set_connection(self, server_list):
-        for server in server_list:
+    def set_connection(self):
+        for server in self.server_list:
             logging.debug("INITIALISING CONNECTION TO " + str(server))
-            self.send_data(server=server)
+            try:
+                self.send_data(server=server)
+            except ConnectionRefusedError:
+                self.heartbeat_counter[server] = 0
 
     def send_data(self, server):
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((server, self.TCP_PORT))
-            s.send(int.to_bytes(self.heartbeat_counter, byteorder="little", length=8))
+            s.send(int.to_bytes(self.heartbeat_counter[server], byteorder="little", length=8))
             s.close()
+            self.heartbeat_counter[server] += 1
+
         except ConnectionAbortedError:
             pass
 
