dafa7291f1b02d22a4c94244c390db8b31ba4877
Maria-Minerva Vonica <maria.vonica98@gmail.com>
2019-11-09 03:07:59 +0200

    Documented the code and refactored a little bit.

--- heartbeat_client.py
+++ heartbeat_client.py
@@ -12,27 +12,49 @@ class HeartbeatClient:
     HEARTBEAT_INCREMENT = 1
 
     def __init__(self):
-        self.heartbeat_counter = {}
-        self.server_list = sys.argv[1:]
+        """
+        Constructor of the class. Initialises the object variables, while also taking the server request lists for the
+        client to connect to.
+        """
+        self.heartbeat_counter = {}  # remember the heartbeat counter for each server in the server list separately
         for server in self.server_list:
             self.heartbeat_counter[server] = 0
+
+        self.server_list = sys.argv[1:]
+
         logging.basicConfig(format='%(levelname)s: %(asctime)s %(message)s', level=logging.INFO)
 
     def start_client(self):
+        """
+        Function which calls the set up connection one to take care of data handling, while also incrementing the
+        heartbeat.
+        :return: None
+        """
         while 1:
             self.set_connection()
-
             time.sleep(self.HEARTBEAT_INCREMENT)
 
     def set_connection(self):
+        """
+        Function which for each server in the server list, tries to send it the heartbeat.
+        Since the client may try to connect to a server without the server being already up, each time the connection
+        fails, the heartbeat is returned to 0. Otherwise, the first heartbeat caught by the server would not be 0.
+        :return: None
+        """
         for server in self.server_list:
             logging.debug("INITIALISING CONNECTION TO " + str(server))
             try:
-                self.send_data(server=server)
+                self.send_heartbeat(server=server)
             except ConnectionRefusedError:
                 self.heartbeat_counter[server] = 0
 
-    def send_data(self, server):
+    def send_heartbeat(self, server):
+        """
+        Function which connects to the server by the given socket, and sends the heartbeat, updating its value for the
+        current server in the server dictionary.
+        :param server: The current server which needs connection.
+        :return: None
+        """
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.connect((server, self.TCP_PORT))
